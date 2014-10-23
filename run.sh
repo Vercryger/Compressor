793a2234c195b3dc3d269859ca15cbea6b478ef8
alan <marioalangonzalez@gmail.com>
2014-10-23 01:13:49 -0300

    First version of encode

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-nasm -f elf library.asm
-nasm -f elf main.asm
-gcc -o test main.c main.o library.o asm_io.o 
+nasm -f elf32 library.asm
+nasm -f elf32 main.asm
+gcc -m32 -o test main.c main.o library.o asm_io.o 
 ./test
