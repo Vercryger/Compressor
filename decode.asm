96b3db221ccf64f41ff54387115f4f5cd779fd79
Vercryger <abuzaid.karim@gmail.com>
2014-10-29 09:43:26 -0300

    Added decode to fix

--- decode.asm
+++ decode.asm
@@ -1,29 +1,63 @@
 ;/*******************************************************************
-; * THIS MODULE IMPLEMENTS THE encode(cadeChar, *matrizCod, *cadeZip)
+; * THIS MODULE IMPLEMENTS THE decode(*cadeZip, *matrizCod, *cadeChar)
 ; * VARIABLES                            
-; * ===> cadeChar at [ebp + 8]                     
+; * ===> *cadeChar at [ebp + 8]                     
 ; * ===> *matrizCod at [ebp + 12]                      
 ; * ===> *cadeZip at [ebp + 16]                    
 ; * ****************************************************************/
-
+%include "asm_io.inc"
 segment .bss
 
 segment .text
 
+  extern getLetter, moveLowToHigh
   global decode
  
 decode:
   enter 0,0
   pusha
 
-;/******************************************************************/
-;/*********************** BEGIN CODIFICATION ***********************/
-;/******************************************************************/
+  mov eax, 0
+  mov ebx, 0
+  mov edi, [ebp + 8]
+  mov esi, [ebp + 16]
 
+  mov bl, [edi]         ; bl = cadeZip[0]
+  while_loop_1:
+    cmp bl, 0 
+    je end_while_1
 
-;/******************************************************************/
-;/************************* END CODIFICATION ***********************/
-;/******************************************************************/
+    push dword ebx
+    call moveLowToHigh
+    add esp, 4
+
+    while_loop_2:
+      cmp bh, 0
+      je end_while_2
+
+      ; fix this condition 
+      shl bh, 1 
+      rcl ax, 1
+
+      new_cod:
+        push dword edi
+        push dword edx
+        call getLetter
+        add esp, 8
+        
+        mov byte [esi], al
+        inc esi
+        mov eax, 0        ; clear the eax
+      continue:
+
+      jmp short while_loop_2
+    end_while_2:
+
+    inc edi
+    mov ebx, 0            ; clear the ebx
+    mov bl, [edi]         ; bl = cadeZip[index++]
+    jmp short while_loop_1
+  end_while_1:
 
   popa
   mov eax, 0
