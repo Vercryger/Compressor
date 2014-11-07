397333afa88a09600667639680263b792615f6e3
Vercryger <abuzaid.karim@gmail.com>
2014-11-06 22:29:36 -0300

    Modified bash script

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ clear
 nasm -f elf32 library.asm
 nasm -f elf32 encode.asm
 nasm -f elf32 decode.asm
-gcc -m32 -o test main.c encode.o decode.o library.o asm_io.o 
-./test
+gcc -m32 -o main main.c encode.o decode.o library.o asm_io.o 
+./main
