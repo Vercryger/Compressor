10d5d808a599fdf5620c81bd5ec59f23efaba471
Vercryger <abuzaid.karim@gmail.com>
2014-11-07 03:16:45 -0300

    Finished decode

--- decode.asm
+++ decode.asm
@@ -5,12 +5,12 @@
 ; * ===> *matrizCod at [ebp + 12]                      
 ; * ===> *cadeChar at [ebp + 16]                    
 ; * ****************************************************************/
-%include "asm_io.inc"
-segment .bss
+segment .data
+  index db 0
 
 segment .text
 
-  extern getLetter, moveLowToHigh
+  extern getLetter
   global decode
  
 decode:
@@ -19,18 +19,18 @@ decode:
 
   mov eax, 0
   mov ebx, 0
+  mov dword [index], 7
   mov edi, [ebp + 8]
   mov esi, [ebp + 16]
 
   mov bh, [edi]                 ; bh = cadeZip[0]
   inc edi
   mov bl, [edi]                 ; bl = cadeZip[1]
-  shl bx, 1                     ; deletes the first 0
+  rol bx, 1                     ; deletes the first 0
   
   while_loop:
-    cmp bh, 255 
-    je end_while
-  
+    
+    dec dword [index]
     shl bx, 1
     jnc new_cod                 ; if (carry == 0) then new_cod 
     rcl al, 1
@@ -48,22 +48,35 @@ decode:
       mov eax, 0                ; clear the eax
     continue:
 
-    cmp bl, 0
-    jne end_if 
+    cmp bx, 32512               ; bh == 127 && bl == 0
+    je end_while
 
+    cmp dword [index], 0                  
+    jne end_if 
       inc edi
       mov bl, [edi]             ; bl = cadeZip[index++]
-      
+
+      mov dword [index], 8      ; index = 8
+
       cmp bl, 0
       jne end_if
-        mov bl, 255
+        mov bl, 127
+
     end_if:
-
+    
     jmp short while_loop
-  end_while:
-
-  dump_regs 255
+  end_while:  
   
+  cmp eax, 0
+  je end
+    push dword [ebp + 12]     ; push *matrizCod
+    push dword eax
+    call getLetter
+    add esp, 8
+
+    mov byte [esi], al
+  end:
+
   popa
   mov eax, 0
   leave
