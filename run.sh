d42d3f9f0a53b38417c08fbe07a962e14bd6033c
Vercryger <abuzaid.karim@gmail.com>
2014-11-08 19:38:42 -0300

    Fixed script

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ nasm -f elf32 library.asm
 nasm -f elf32 encode.asm
 nasm -f elf32 decode.asm
 gcc -m32 -o main main.c encode.o decode.o library.o asm_io.o 
+# Provided example
 ./main "ADDCCBBACCCBBAADABB"
